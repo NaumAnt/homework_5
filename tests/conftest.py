5b94ab62e533e405d17ad7be25fefd77b8ba3966
Anton Naumenko <Naumenko_anton_pnz@mail.ru>
2025-06-22 20:33:23 +0300

    Добавил папку с файлом "form_send" для инкапсуляции деталей бизнесс шагов. Изменил выполнил рефакторинг основного теста.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ def setup_browser():
     # Устанавливаем размер окна браузера
     browser.config.window_width = 1950
     browser.config.window_height = 1250
+    yield browser
+    browser.quit()
+
+@pytest.fixture
+def open_form_browser_chrome(setup_browser):
+    setup_browser.open('https://demoqa.com/automation-practice-form')
--- tests/test_form_send.py
+++ tests/test_form_send.py
@@ -1,41 +1,30 @@
+from pages.form_send import RegistrationPage
+
+
 import os
 from selene import browser, have
 
-def test_form_send(setup_browser):
-    browser.open("https://demoqa.com/automation-practice-form")
-    browser.element('#firstName').type('Sergey')
-    browser.element('#lastName').type('Druzhko')
-    browser.element('#userEmail').type('test@test.com')
-    browser.element('.custom-control-label').click()
-    browser.element('#userNumber').type('1233211234')
+def test_form_send(open_form_browser_chrome):
 
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').element('option[value="1998"]').click()
-    browser.element('.react-datepicker__month-select').element('option[value="5"]').click()
-    browser.element('[aria-label="Choose Wednesday, July 8th, 1998"]').click()
+    form_send = RegistrationPage()
 
-    browser.element('#subjectsInput').type('econom').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
+    form_send.fill_first_name('Sergey')
+    form_send.fill_last_name('Druzhko')
+    form_send.fill_user_email('test@test.com')
+    form_send.fill_gender('Male')
+    form_send.fill_user_number('1233211234')
+    form_send.fill_date_of_birth('1998', 'July', '08' )
+    form_send.fill_hobbies('Sports')
+    form_send.fill_picture('imagetest.png')
+    form_send.fill_current_address('ул. Пушкина. д.100')
+    form_send.fill_subjects('Economics')
+    form_send.fill_state('Rajasthan')
+    form_send.fill_city('Jaipur')
+    form_send.fill_submit()
 
-    image = 'imagetest.png'
-    browser.element('#uploadPicture').send_keys(os.path.abspath(image))
-
-    browser.element('#currentAddress').type('ул. Пушкина. д.100')
-    browser.element('#react-select-3-input').type('raja').press_enter()
-    browser.element('#react-select-4-input').type('jaip').press_enter()
-
-    browser.element('#submit').click()
-
-    browser.element('tbody').should(have.text('Student Name Sergey Druzhko'))
-    browser.element('tbody').should(have.text('Student Email test@test.com'))
-    browser.element('tbody').should(have.text('Gender Male'))
-    browser.element('tbody').should(have.text('Mobile 1233211234'))
-    browser.element('tbody').should(have.text('Date of Birth 08 July,1998'))
-    browser.element('tbody').should(have.text('Subjects Economics'))
-    browser.element('tbody').should(have.text('Hobbies Sports'))
-    browser.element('tbody').should(have.text('Picture imagetest.png'))
-    browser.element('tbody').should(have.text('Address ул. Пушкина. д.100'))
-    browser.element('tbody').should(have.text('State and City Rajasthan Jaipur'))
+    form_send.check_registered_user('Sergey Druzhko', 'test@test.com', 'Male', '1233211234', '08 July,1998',
+                                    'Economics', 'Sports', 'imagetest.png', 'ул. Пушкина. д.100',
+                                    'Rajasthan Jaipur')
 
 
 
