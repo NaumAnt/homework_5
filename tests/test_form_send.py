0ab9fbabede0da422a75da34cb13734aa824a106
Anton Naumenko <Naumenko_anton_pnz@mail.ru>
2025-07-22 02:07:19 +0300

    Изменил имеющийся тест

--- tests/test_form_send.py
+++ tests/test_form_send.py
@@ -1,43 +1,44 @@
 import os
 import allure
 from allure_commons.types import Severity
-from selene import browser, have
+from selene import Browser, Config, have
+
 
 def test_form_send(setup_browser):
-    browser.open("https://demoqa.com/automation-practice-form")
-    browser.element('#firstName').type('Sergey')
-    browser.element('#lastName').type('Druzhko')
-    browser.element('#userEmail').type('test@test.com')
-    browser.element('.custom-control-label').click()
-    browser.element('#userNumber').type('1233211234')
+    setup_browser.open("https://demoqa.com/automation-practice-form")
+    setup_browser.element('#firstName').type('Sergey')
+    setup_browser.element('#lastName').type('Druzhko')
+    setup_browser.element('#userEmail').type('test@test.com')
+    setup_browser.element('.custom-control-label').click()
+    setup_browser.element('#userNumber').type('1233211234')
 
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').element('option[value="1998"]').click()
-    browser.element('.react-datepicker__month-select').element('option[value="5"]').click()
-    browser.element('[aria-label="Choose Wednesday, July 8th, 1998"]').click()
+    setup_browser.element('#dateOfBirthInput').click()
+    setup_browser.element('.react-datepicker__year-select').element('option[value="1998"]').click()
+    setup_browser.element('.react-datepicker__month-select').element('option[value="5"]').click()
+    setup_browser.element('[aria-label="Choose Wednesday, July 8th, 1998"]').click()
 
-    browser.element('#subjectsInput').type('econom').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
+    setup_browser.element('#subjectsInput').type('econom').press_enter()
+    setup_browser.element('[for="hobbies-checkbox-1"]').click()
 
     image = 'imagetest.png'
-    browser.element('#uploadPicture').send_keys(os.path.abspath(image))
+    setup_browser.element('#uploadPicture').send_keys(os.path.abspath(image))
 
-    browser.element('#currentAddress').type('ул. Пушкина. д.100')
-    browser.element('#react-select-3-input').type('raja').press_enter()
-    browser.element('#react-select-4-input').type('jaip').press_enter()
+    setup_browser.element('#currentAddress').type('ул. Пушкина. д.100')
+    setup_browser.element('#react-select-3-input').type('raja').press_enter()
+    setup_browser.element('#react-select-4-input').type('jaip').press_enter()
 
-    browser.element('#submit').click()
+    setup_browser.element('#submit').click()
 
-    browser.element('tbody').should(have.text('Student Name Sergey Druzhko'))
-    browser.element('tbody').should(have.text('Student Email test@test.com'))
-    browser.element('tbody').should(have.text('Gender Male'))
-    browser.element('tbody').should(have.text('Mobile 1233211234'))
-    browser.element('tbody').should(have.text('Date of Birth 08 July,1998'))
-    browser.element('tbody').should(have.text('Subjects Economics'))
-    browser.element('tbody').should(have.text('Hobbies Sports'))
-    browser.element('tbody').should(have.text('Picture imagetest.png'))
-    browser.element('tbody').should(have.text('Address ул. Пушкина. д.100'))
-    browser.element('tbody').should(have.text('State and City Rajasthan Jaipur'))
+    setup_browser.element('tbody').should(have.text('Student Name Sergey Druzhko'))
+    setup_browser.element('tbody').should(have.text('Student Email test@test.com'))
+    setup_browser.element('tbody').should(have.text('Gender Male'))
+    setup_browser.element('tbody').should(have.text('Mobile 1233211234'))
+    setup_browser.element('tbody').should(have.text('Date of Birth 08 July,1998'))
+    setup_browser.element('tbody').should(have.text('Subjects Economics'))
+    setup_browser.element('tbody').should(have.text('Hobbies Sports'))
+    setup_browser.element('tbody').should(have.text('Picture imagetest.png'))
+    setup_browser.element('tbody').should(have.text('Address ул. Пушкина. д.100'))
+    setup_browser.element('tbody').should(have.text('State and City Rajasthan Jaipur'))
 
 
 
